fbcee32652380454492d2f4c4c1d7cddbdd4eead
Woody Aung <waung5@berkeley.edu>
2026-01-01 23:30:12 -0800

    typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import {BrowserRouter,Routes,Route,Navigate} from "react-router-dom"
-import Login from "./pages/login"
+import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
